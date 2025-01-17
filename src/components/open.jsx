05ba7753252e79f681fd34ab52ec38438894b7fb
HesamPourabbasian <137700266+HesamPourabbasian@users.noreply.github.com>
2025-01-17 19:06:24 +0330

    Update open.jsx

--- src/components/open.jsx
+++ src/components/open.jsx
@@ -1,6 +1,6 @@
 const Open = () => {
   return (
-    <div className="relative h-[600px] bg-[url('public/g.gif')] bg-no-repeat bg-cover bg-center flex items-center justify-center">
+    <div className="relative h-[600px] bg-[url('/g.gif')] bg-no-repeat bg-cover bg-center flex items-center justify-center">
       <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
       {/* Dark overlay */}
       <h1 className="text-white text-4xl sm:text-5xl md:text-6xl font-semibold z-10 text-center">
